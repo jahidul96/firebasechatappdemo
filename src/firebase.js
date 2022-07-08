daaa5ca58217c3c6cefb5d98b679b545f3ad7cd2
Jahidul Islam <97932459+jahidul96@users.noreply.github.com>
2022-07-08 14:53:44 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
     //    api key here
-    apiKey: "AIzaSyCeSQ6z-fwZNJ6U7UDFhqQIdHh6tkmlEWE",
-    authDomain: "tuorgang.firebaseapp.com",
-    projectId: "tuorgang",
-    storageBucket: "tuorgang.appspot.com",
-    messagingSenderId: "186485655222",
-    appId: "1:186485655222:web:ec89cdf59e4f0fef468817"
+  
 
 };
 
